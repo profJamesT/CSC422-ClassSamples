45c0fbdfea0cc09655c48e28c2de2ca68f60d178
jamestucker <jtucker@csp.edu>
2020-04-18 14:12:30 -0400

    Initial Project

---
+++ mavenproject1/SimpleGithubProject/src/main/java/edu/csp/simplegithubproject/Week1Sample.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.csp.simplegithubproject;
+
+/**
+ *
+ * @author jamestucker
+ */
+public class Week1Sample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello GitHub!");
+    }
+    
+}
