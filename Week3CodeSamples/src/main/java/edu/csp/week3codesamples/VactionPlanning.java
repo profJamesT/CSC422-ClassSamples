220c8d56a13410c9a6221ffc532930c0dbdc8ea0
jamestucker <jtucker@csp.edu>
2020-05-26 12:54:46 -0400

    New Project

---
+++ Week3CodeSamples/src/main/java/edu/csp/week3codesamples/VactionPlanning.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.csp.week3codesamples;
+
+/**
+ *
+ * @author jamestucker
+ */
+public class VactionPlanning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
