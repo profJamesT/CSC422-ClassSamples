a2c622a96c7ce8759b4c55f818358158ad6664df
jamestucker <jtucker@csp.edu>
2020-05-26 15:43:20 -0400

    Updated Saving and Loading from file

--- Week3CodeSamples/src/main/java/edu/csp/week3codesamples/VactionPlanning.java
+++ Week3CodeSamples/src/main/java/edu/csp/week3codesamples/VactionPlanning.java
@@ -6,11 +6,15 @@
 package edu.csp.week3codesamples;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,7 +25,7 @@ public class VactionPlanning {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         ArrayList<Trip> tripList = new ArrayList<Trip>();
         Trip tripOne = new Trip("Hawaii");
         Trip tripTwo = new Trip("Alaska");
@@ -34,18 +38,32 @@ public class VactionPlanning {
         tripList.add(tripThree);
         
         //save the trips
-  
+        try (ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream("James.ser", false))) {
+            oout.writeObject(tripList);
+        } catch (FileNotFoundException ex) {
+            System.out.println("FileNotFoundException: " + ex);
+        } catch (IOException ex) {
+            System.out.println("IOExcption: " + ex);
+        }
   
         //init the tripList before loading to make sure its actually loading
         tripList = null;
         
         //load the trips
-
+        try (ObjectInputStream oin = new ObjectInputStream(new FileInputStream("James.ser"))) {
+          tripList = (ArrayList<Trip>)oin.readObject();
+        } catch (FileNotFoundException ex) {
+            System.out.println("FileNotFoundException: " + ex);
+        } catch (IOException ex) {
+            System.out.println("IOExcption: " + ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException: " + ex);
+        }
         
         for (Trip t : tripList) {
             System.out.println(t.getTripName() + " - " + t.getLocations().size());
         }
         
-    }
+    }   
     
 }
