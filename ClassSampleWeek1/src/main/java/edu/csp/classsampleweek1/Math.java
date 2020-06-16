34788817576ed8b420d35eda5d060ddab4ebc21e
James Tucker <jtucker@csp.edu>
2020-06-16 07:59:22 -0400

    New Math Class for JUnit testing

---
+++ ClassSampleWeek1/src/main/java/edu/csp/classsampleweek1/Math.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.csp.classsampleweek1;
+
+/**
+ *
+ * @author james
+ */
+public class Math {
+    
+    public int squareIt(int x) {
+        return x * x;
+    }
+    
+    public boolean isEven(int x) {
+        if (x%2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public int sumIt(int x, int y) {
+        return x * y;
+    }
+    
+     
+}
